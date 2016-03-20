90895378acde2d3d7d96e263dd4209e303bc90fa
NinjaWolf64 <jsmerrill2004@gmail.com>
2016-03-20 16:17:02 -0700

    Create code1.py

---
+++ code1.py
@@ -0,0 +1,12 @@
+from time import sleep
+print('Code 1')
+input('Press any key to continue')
+boolean1 = True
+i = 1
+while boolean1
+	print('This has run' + str(i) + 'times')
+	sleep(0.25)
+	if i > 400
+		boolean1 = False
+	i += 1
+print('Done)
